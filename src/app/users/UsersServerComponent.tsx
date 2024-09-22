b5578f38f91ed2a8e848b67148d0e722bcf29a80
Dmitriy Dmovskiy <dmovskidv@gmail.com>
2024-09-22 18:12:45 +0300

    Fixed deploy.

--- src/app/users/UsersServerComponent.tsx
+++ src/app/users/UsersServerComponent.tsx
@@ -1,4 +1,4 @@
-const fetchUsers = async (): Promise<any[]> => {
+const fetchUsers = async (): Promise<unknown[]> => {
     const data = await fetch('https://jsonplaceholder.typicode.com/users');
     return data.json();
 };
--- src/app/users/UsersWrapper.tsx
+++ src/app/users/UsersWrapper.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
 const UsersWrapper = ({ children }) => {
    return <div>{children}</div>
 }
